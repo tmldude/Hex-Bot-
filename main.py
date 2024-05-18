0e8cbc80cf9670c667b571487eff7637f65c3431
Tmldude <tl688@cornell.edu>
2024-05-18 03:58:55 -0400

    visualization stuff

--- main.py
+++ main.py
@@ -13,17 +13,43 @@ model_path2 = r"C:\Users\tmlaz\Desktop\chesspy\models\chess_model_250k.h5"
 optimizer_path = r"C:\Users\tmlaz\Desktop\chesspy\chess_cnn_optimizer_250k.pth"
 
 def simulate_games(num_games=5):    
+
+    max_moves = 256
+    centipawn_loss_data_mini = np.zeros((num_games, max_moves))
+
+    for game in range(num_games):
+        engine = Engine(chess.STARTING_BOARD_FEN,
+                 decisions=0,
+                 model_type=0,
+                 model_path=model_path,
+                 sf_level=7,
+                 sf_path=engine_path,
+                 depth=1)
+
+        print(f"Simulating game {game + 1} / {num_games}")
+        centipawn_losses = engine.play_stockfish()
+        for i in range(min(len(centipawn_losses), max_moves)):
+            centipawn_loss_data_mini[game, i] = centipawn_losses[i]
+
+    # Calculate the average centipawn loss per move
+    average_centipawn_loss_per_move_mini = np.mean(centipawn_loss_data_mini, axis=0)
+
+    # Save the data
+    np.save('centipawn_loss_data_50k_mini.npy', centipawn_loss_data_mini)
+    np.save('average_centipawn_loss_per_move_50k_mini.npy',
+            average_centipawn_loss_per_move_mini)
+
     max_moves = 256
     centipawn_loss_data_nega = np.zeros((num_games, max_moves))
 
     for game in range(num_games):
         engine = Engine(chess.STARTING_BOARD_FEN,
                  decisions=1,
-                 model_type=1,
-                 model_path=model_path2,
+                 model_type=0,
+                 model_path=model_path,
                  sf_level=7,
                  sf_path=engine_path,
-                 depth=1)
+                 depth=3)
 
         print(f"Simulating game {game + 1} / {num_games}")
         centipawn_losses = engine.play_stockfish()
@@ -38,16 +64,14 @@ def simulate_games(num_games=5):
     np.save('average_centipawn_loss_per_move_50k_nega.npy',
             average_centipawn_loss_per_move_nega)
     
-    average_centipawn_loss_per_move_mini = np.load('average_centipawn_loss_per_move_50k_mini.npy')
-
-    # Plot the average centipawn loss per move
-    plt.plot(range(1, 50), average_centipawn_loss_per_move_mini)
-    plt.plot(range(1, 50), average_centipawn_loss_per_move_nega)
-    plt.xlabel('Move Number')
-    plt.ylabel('Average Centipawn Loss')
-    plt.title('Average Centipawn Loss Per Move Over 25 Games')
-    plt.grid(True)
-    plt.show()
+    # # Plot the average centipawn loss per move
+    # plt.plot(range(1, 50), average_centipawn_loss_per_move_mini)
+    # plt.plot(range(1, 50), average_centipawn_loss_per_move_nega)
+    # plt.xlabel('Move Number')
+    # plt.ylabel('Average Centipawn Loss')
+    # plt.title('Average Centipawn Loss Per Move Over 25 Games')
+    # plt.grid(True)
+    # plt.show()
 
 
 import numpy as np
@@ -57,7 +81,7 @@ import matplotlib.pyplot as plt
 
 
 def main():
-
+    simulate_games()
     average_centipawn_loss_per_move_mini = np.load('average_centipawn_loss_per_move_50k_mini.npy')
     average_centipawn_loss_per_move_nega = np.load('average_centipawn_loss_per_move_50k_nega.npy')
 
